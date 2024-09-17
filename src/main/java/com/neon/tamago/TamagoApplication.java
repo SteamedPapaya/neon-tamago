9e99230a2ced1873f85e2ed75b9fa2bfdd3a8b69
hyeongjun <sx0527@naver.com>
2024-09-17 23:31:36 +0900

    init

---
+++ src/main/java/com/neon/tamago/TamagoApplication.java
@@ -0,0 +1,13 @@
+package com.neon.tamago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TamagoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TamagoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/neon/tamago/TamagoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neon.tamago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TamagoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
