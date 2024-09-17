d13af02bed821b3ff4d21754087386100c4b0e4e
siyoon <ryuu.public@gmail.com>
2024-09-17 23:47:30 +0900

    chore: setup backend project structure and add basic entities and repositories

---
+++ src/main/java/com/neon/tamago/repository/TicketRepository.java
@@ -0,0 +1,9 @@
+package com.neon.tamago.repository;
+
+import com.neon.tamago.model.Ticket;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TicketRepository extends JpaRepository<Ticket, Long> {
+}
---
+++ src/main/java/com/neon/tamago/service/EventService.java
@@ -0,0 +1,23 @@
+package com.neon.tamago.service;
+
+import com.neon.tamago.model.Event;
+import com.neon.tamago.repository.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EventService {
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    public List<Event> getAllEvents() {
+        return eventRepository.findAll();
+    }
+
+    public Event saveEvent(Event event) {
+        return eventRepository.save(event);
+    }
+}
